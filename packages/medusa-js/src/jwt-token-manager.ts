bfa02d4b47568c5a53fb7bc9efe3239ab9d1ab87
Shrinjay Mukherjee <shrinjaymukherjee@gmail.com>
2023-11-01 16:46:00 -0400

    update jwt

--- packages/medusa-js/src/jwt-token-manager.ts
+++ packages/medusa-js/src/jwt-token-manager.ts
@@ -11,8 +11,16 @@ class JwtTokenManager {
   public registerJwt(token: string, domain: "admin" | "store") {
     if (domain === "admin") {
       this.adminJwt = token;
+
+      if (sessionStorage) {
+        sessionStorage.setItem(`medusa:jwt:${domain}`, token);
+      }
     } else if (domain === "store") {
       this.storeJwt = token;
+
+      if (sessionStorage) {
+        sessionStorage.setItem(`medusa:jwt:${domain}`, token);
+      }
     } else {
       throw new Error(`'domain' must be wither 'admin' or 'store' received ${domain}`)
     }
@@ -23,8 +31,16 @@ class JwtTokenManager {
    */
   public getJwt(domain: "admin" | "store") {
     if (domain === "admin") {
+      if (sessionStorage.getItem(`medusa:jwt:${domain}`) && !this.adminJwt) {
+        this.adminJwt = sessionStorage.getItem(`medusa:jwt:${domain}`);
+      }
+
       return this.adminJwt;
     } else if (domain === "store") {
+      if (sessionStorage.getItem(`medusa:jwt:${domain}`) && !this.storeJwt) {
+        this.storeJwt = sessionStorage.getItem(`medusa:jwt:${domain}`);
+      }
+
       return this.storeJwt;
     } else {
       throw new Error(`'domain' must be wither 'admin' or 'store' received ${domain}`)
